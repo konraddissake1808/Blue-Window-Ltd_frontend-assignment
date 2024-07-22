ead66a1b4fe0bd2b22dfdb0035fd1eab1c46b6ac
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-07-23 00:03:05 +0100

    Installed font awesome

--- blue-window-ltd-assignment/src/app/components/Casino/index.tsx
+++ blue-window-ltd-assignment/src/app/components/Casino/index.tsx
@@ -3,7 +3,7 @@ import GrosvenorCasino from "../../../../public/Grosvenor-casinos.png"
 import HappySpins from "../../../../public/Happy-spins.png"
 import Hajpers from "../../../../public/Hajpers.png"
 import Image from 'next/image';
-import { StaticImageData } from 'next/image';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 interface CasinoProps {
   title?: string;
@@ -16,31 +16,44 @@ interface CasinoProps {
 const Casino: React.FC<CasinoProps> = ({image, id, title, rating}) => {
     if (id=="0001"){
         return (
-            <div className='text-black'>
-                <div>
-                    <Image src={GrosvenorCasino} alt=''/>
+            <div className='casinoCard'>
+                <div className='text-black flex justify-center flex-col p-4 rounded shadow-[0px_0px_20px_0px_rgba(0, 0, 0, 0.1)]'>
+                    <div className='h-[117px] flex justify-center items-center overflow-hidden bg-[#03252B]'>
+                        <Image className='bg-contain bg-center h-[133px] w-[133px]' src={GrosvenorCasino} alt=''/>
+                    </div>
+                    <div>
+                        <p className='text-center'>{title}</p>
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
-                <p>{title}</p>
             </div>
         );
     }
     else if (id=="0002") {
         return(
-            <div className='text-black'>
-                <div>
-                    <Image src={HappySpins} alt=''/>
+            <div className='casinoCard'>
+                <div className='text-black p-4 rounded shadow-[0px_0px_20px_0px_rgba(0, 0, 0, 0.1)]'>
+                    <div className='h-[117px] flex justify-center items-center overflow-hidden bg-[#43082F]'>
+                        <Image className=' bg-center bg-contain h-[133px] w-[133px]' src={HappySpins} alt=''/>
+                    </div>
+                    <p className='text-center'>{title}</p>
                 </div>
-                <p>{title}</p>
             </div>
         )
     }
     else if (id=="0003") {
         return(
-            <div className='text-black'>
-                <div>
-                    <Image src={Hajpers} alt=''/>
+            <div className='casinoCard'>
+                <div className='text-black p-4 rounded'>
+                    <div className='h-[117px] flex justify-center items-center overflow-hidden bg-[#123156]'>
+                        <Image className='bg-contain bg-center h-[133px] w-[133px]' src={Hajpers} alt=''/>
+                    </div>
+                    <div>
+                        <p className='text-center font-sans'>{title}</p>
+                    </div>
                 </div>
-                <p>{title}</p>
             </div>
         )
     }
--- blue-window-ltd-assignment/src/app/page.tsx
+++ blue-window-ltd-assignment/src/app/page.tsx
@@ -2,21 +2,18 @@ import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import Casino from "./components/Casino/index";
 
-const montserrat = Montserrat({ 
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-})
-
 export default function Home() {
   return (
-    <main className="flex w-full flex-col items-center justify-between">
+    <main className="flex w-full flex-col items-center justify-between font-sans">
       <div className="z-10 w-full mt-[60px] px-5 max-w-5xl items-center justify-between font-sans text-sm lg:flex">
         <div>
-          <p className="font-bold text-2xl text-[#939393]">Best Casinos 2024</p>
+          <div>
+            <p className="font-bold text-2xl text-[#939393]">Best Casinos 2024</p>
+          </div>
+          <Casino image="Grosvenor" id="0001" title="Grosvenor Casinos" rating={5}/>
+          <Casino image="Happy-spins" id="0002" title="Happy Spins" rating={5}/>
+          <Casino image="Hajpers" id="0003" title="Hajpers" rating={5}/>
         </div>
-        <Casino image="Grosvenor" id="0001" title="Grosvenor Casinos" rating={5}/>
-        <Casino image="Happy-spins" id="0002" title="Happy Spins" rating={5}/>
-        <Casino image="Hajpers" id="0003" title="Hajpers" rating={5}/>
       </div>
     </main>
   );
