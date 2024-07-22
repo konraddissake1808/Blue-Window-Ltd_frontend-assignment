cd1c2ce1517e72349cd3f3a8a0e1310a24fa5f8c
konraddissake1808 <konrad.dissakengando@gmail.com>
2024-07-22 18:33:56 +0100

    Import the images in the UI

--- blue-window-ltd-assignment/src/app/components/Casino/index.tsx
+++ blue-window-ltd-assignment/src/app/components/Casino/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import GrosvenorCasino from "../../../../public/Grosvenor-casinos.png"
+import HappySpins from "../../../../public/Happy-spins.png"
+import Hajpers from "../../../../public/Hajpers.png"
 import Image from 'next/image';
 import { StaticImageData } from 'next/image';
 
@@ -10,15 +12,39 @@ interface CasinoProps {
   rating?: number;
 }
 
+
 const Casino: React.FC<CasinoProps> = ({image, id, title, rating}) => {
-  return (
-    <div className='text-black'>
-        <div>
-            
-        </div>
-        <p>{title}</p>
-    </div>
-  )
-}
+    if (id=="0001"){
+        return (
+            <div className='text-black'>
+                <div>
+                    <Image src={GrosvenorCasino} alt=''/>
+                </div>
+                <p>{title}</p>
+            </div>
+        );
+    }
+    else if (id=="0002") {
+        return(
+            <div className='text-black'>
+                <div>
+                    <Image src={HappySpins} alt=''/>
+                </div>
+                <p>{title}</p>
+            </div>
+        )
+    }
+    else if (id=="0003") {
+        return(
+            <div className='text-black'>
+                <div>
+                    <Image src={Hajpers} alt=''/>
+                </div>
+                <p>{title}</p>
+            </div>
+        )
+    }
+  }
+
 
 export default Casino
